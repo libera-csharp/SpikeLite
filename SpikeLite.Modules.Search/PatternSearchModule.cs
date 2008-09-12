1d512bd364d6b1358c389a29cb290927e62048af
Andy Jump <andyjump@hotmail.co.uk>
2008-09-12 13:10:38 +0000

    Added pattern search module that searches www.dofactory.com/Patterns/

---
+++ SpikeLite.Modules.Search/PatternSearchModule.cs
@@ -0,0 +1,22 @@
+/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2008 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+using SpikeLite.AccessControl;
+
+namespace SpikeLite.Modules.Search
+{
+    [Module("Pattern", "Search Refactoring.com", "~pattern <search terms>", AccessLevel.Public)]
+    public class PatternSearchModule : SiteSearchModuleBase
+    {
+        #region Constructors
+        public PatternSearchModule()
+            : base("Pattern", "pattern", "www.dofactory.com/Patterns/")
+        {
+        }
+        #endregion
+    }
+}
