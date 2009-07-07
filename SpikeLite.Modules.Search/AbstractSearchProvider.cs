3ed744651576c54f1582a321d6498051306259cc
Greg F <kog@epiphanic.org>
2009-07-07 09:30:04 +0000

    Whoops, missing a file.

---
+++ SpikeLite.Modules.Search/AbstractSearchProvider.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2009 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+ 
+using System;
+
+namespace SpikeLite.Modules.Search
+{
+    /// <summary>
+    /// An abstract class for our search providers, allowing for some wiring and simple re-use.
+    /// </summary>
+    public abstract class AbstractSearchProvider : ISearchProvider
+    {
+        public string ApiKey
+        {
+            get; set;
+        }
+
+        public abstract void ExecuteSearch(string searchCriteria, string domain, Action<string[]> callbackHandler);
+    }
+}
