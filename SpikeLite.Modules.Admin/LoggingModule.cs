559dbb382fca4e0389f10a424907e92099334285
Greg F <kog@epiphanic.org>
2009-07-14 08:08:07 +0000

    Adds an administrative module to tweak the Log4Net threshold at runtime. You can already do this via your log4net.xml at runtime (it's set to FILE-WATCH), but only if you have immediate shell (or insert remote administration tool here) access.
    One side effect is that you cannot at present use the module to set TRACE level - only DEBUG, INFO and WARN. This is due to the mechanism that we use for doing TRACE. 
    
    Another side effect worth noting is that now the log file appends, where it did not before. This is due to the XmlConfigurator re-opening the log file after a rehash, which with append set to false causes the log file to truncate any activity to present, which is probably not advised. If you desire this, please override the log4net.xml file and tell the logger not to append. Lastly you might wish to change your startup scripts to do an "rm" or "del" on the spikelite.log so it doesn't grow out of hand. Or alter the rolling appender params, giving it a max size.
    
    The console will be the appender that, by default, receives DEBUG messages. The log file still only receives INFO+ level, so if you're wondering where your lovely DEBUG level went then look at your console. Further, the new message interceptor for PRIVMSG works at the trace level - so you'll still have to modify the log4net.xml to get this.

---
+++ SpikeLite.Modules.Admin/LoggingModule.cs
@@ -0,0 +1,72 @@
+﻿/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2009 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using log4net.Config;
+using log4net.Core;
+using log4net.Ext.Trace;
+using log4net.Repository.Hierarchy;
+using SpikeLite.Communications;
+using SpikeLite.Domain.Model.Authentication;
+using Mono.Rocks;
+
+namespace SpikeLite.Modules.Admin
+{
+    /// <summary>
+    /// This class is an administrative module that allows users with <see cref="AccessLevel.Root"/> access to change the logging treshold of all appenders. Valid arguments
+    /// are <code>DEBUG</code>, <code>INFO</code> and <code>WARN</code>
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// Due to the nature of how TRACE level logging is implemented, we do not currently support dynamically changing to TRACE level logging. To do so please restart the
+    /// bot and modify your app.config.
+    /// </remarks>
+    public class LoggingModule : ModuleBase
+    {
+        /// <summary>
+        /// Holds a logger which we can, amusingly enough, use for logging treshold changes to our loggers.
+        /// </summary>
+        private readonly TraceLogImpl _logger = (TraceLogImpl) TraceLogManager.GetLogger(typeof (LoggingModule));
+
+        /// <summary>
+        /// Holds a mapping of level name -> level entry. We use this because for some reason the Log4Net folks use a class instead of an enum.
+        /// </summary>
+        private readonly Dictionary<string, Level> _loggingLevels = new Dictionary<string, Level> { {"debug", Level.Debug}, {"info", Level.Info}, {"warn", Level.Warn}};
+
+        public override void HandleRequest(Request request)
+        {
+            string[] message = request.Message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (request.RequestFrom.AccessLevel >= AccessLevel.Root
+                && message[0].Equals("~logging", StringComparison.OrdinalIgnoreCase)
+                && message.Length == 2)
+            {
+                string levelName = message[1].ToLower();
+
+                if (_loggingLevels.ContainsKey(levelName))
+                {
+                    Level loggingLevel = _loggingLevels[levelName];
+
+                    LoggerManager.GetAllRepositories().ForEach(x =>
+                    {
+                        x.Threshold = loggingLevel;
+                        x.GetCurrentLoggers().ForEach(y => ((Logger)y).Level = loggingLevel);
+                    });
+
+                    ((Hierarchy)log4net.LogManager.GetRepository()).Root.Level = loggingLevel;
+                    XmlConfigurator.Configure(new FileInfo("log4net.xml"));
+
+                    // You may not actually catch this if you've just set the level to WARN, but if you're doing that type of filtering chances are you don't care anyway.
+                    _logger.InfoFormat("Set logging level to {0}.", loggingLevel.DisplayName);
+                }
+            }
+        }
+    }
+}
