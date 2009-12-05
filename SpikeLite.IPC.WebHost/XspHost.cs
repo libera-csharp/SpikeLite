d0a135836b6c95329f11dfe12698e3037f8ac4f0
Greg F <kog@epiphanic.org>
2009-12-05 12:40:21 +0000

    XSP module now understands start/stop commands. Shutting down the ApplicationServer for XSP does seem to take a minute or two, not quite sure what to do about that since there's no fine-grained control or diagnostics. Might also look into making it a BG thread.

--- SpikeLite.IPC.WebHost/XspHost.cs
+++ SpikeLite.IPC.WebHost/XspHost.cs
@@ -6,9 +6,11 @@
  * distributed license.txt for details.
  */
 
+using System;
 using System.Net;
 using Mono.WebServer;
 using SpikeLite.Communications;
+using SpikeLite.Domain.Model.Authentication;
 using SpikeLite.Modules;
 
 // TODO: Kog 12/5/2009 - Maybe we can wire this up w/ Spring.NET somehow?
@@ -20,7 +22,6 @@ namespace SpikeLite.IPC.WebHost
     /// </summary>
     public class XspHost : ModuleBase
     {
-
         #region Data Members and Properties 
 
         /// <summary>
@@ -37,6 +38,14 @@ namespace SpikeLite.IPC.WebHost
         /// Holds a reference to the application server we're about to bind.
         /// </summary>
         private readonly ApplicationServer _appServer;
+
+        // TODO: Kog 12/5/2009 - It doesn't look like there's any way to query XSP to see what it's doing... so for now I just hacked out a boolean. This requires
+        // TODO: Kog 12/5/2009 - more work.
+
+        /// <summary>
+        /// Stores whether or not the appserver has been spun up. Primarily useful for doing start/stop operations.
+        /// </summary>
+        private bool _isAppServerStarted;
 
         /// <summary>
         /// Gets or sets the port that we should bind to. This defaults to 8080.
@@ -63,6 +72,9 @@ namespace SpikeLite.IPC.WebHost
 
         #endregion
 
+        /// <summary>
+        /// Constructs our module, configures XSP2 and launches it.
+        /// </summary>
         public XspHost()
         {
             // TODO: Kog 12/5/2009 - Add configuration for IPs... I think we can leave this at ANY for now though.
@@ -70,12 +82,70 @@ namespace SpikeLite.IPC.WebHost
             _appServer.AddApplicationsFromCommandLine(string.Format("{0}:/:{1}", Port, Path));
 
             // TODO: Kog 12/5/2009 - Do we want this to be done by parsing messages below, or to auto start?
-            _appServer.Start(true);
+            StartAppServer();
         }
 
         public override void HandleRequest(Request request)
         {
-            // TODO: Kog 12/5/2009 - Consume a stop message, such that we can shut down XSP without killing the bot.
+            if (request.RequestFrom.AccessLevel >= AccessLevel.Root && request.Message.StartsWith("~xsp", StringComparison.OrdinalIgnoreCase))
+            {
+                string[] splitMessage = request.Message.Split(' ');
+                string operationDescription = splitMessage[1];
+
+                if (operationDescription.Equals("start", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Make sure we're not started already.
+                    if (!_isAppServerStarted)
+                    {
+                        StartAppServer();
+
+                        Logger.InfoFormat("{0} started the XSP server.", request.Nick);
+                        ModuleManagementContainer.HandleResponse(request.CreateResponse(ResponseType.Public, "XSP server started."));
+                    }
+                    else
+                    {
+                        ModuleManagementContainer.HandleResponse(request.CreateResponse(ResponseType.Public, "The XSP server is already running, cannot start it."));
+                    }
+                }
+
+                if (operationDescription.Equals("stop", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Make sure we're started already. 
+                    if (_isAppServerStarted)
+                    {
+                        StopAppServer();
+
+                        Logger.InfoFormat("{0} stopped the XSP server.", request.Nick);
+                        ModuleManagementContainer.HandleResponse(request.CreateResponse(ResponseType.Public, "XSP server stopping..."));
+                    }
+                    else
+                    {
+                        ModuleManagementContainer.HandleResponse(request.CreateResponse(ResponseType.Public, "The XSP server must already be running to stop it."));    
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Attempts to start the XSP app server.
+        /// </summary>
+        private void StartAppServer()
+        {
+            _appServer.Start(true);
+            _isAppServerStarted = true;
+        }
+
+        /// <summary>
+        /// Attempts to stop the XSP app server.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// It's worth noting that there's a significant time laps between calling Stop() and the actual appserver stopping.
+        /// </remarks>
+        private void StopAppServer()
+        {
+            _appServer.Stop();
+            _isAppServerStarted = false;
         }
     }
 }
