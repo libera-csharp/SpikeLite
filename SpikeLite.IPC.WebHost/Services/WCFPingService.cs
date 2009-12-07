9eb2ad7698a891c3816c9f55d174ba39f74dbfca
Greg F <kog@epiphanic.org>
2009-12-07 05:08:00 +0000

    Swap out XSP in favor of WCF endpoints, as Mono apparently now supports these. Thanks to Bojan R.

---
+++ SpikeLite.IPC.WebHost/Services/WCFPingService.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2009 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+
+using System.ServiceModel;
+using SpikeLite.Communications;
+using SpikeLite.Modules;
+
+namespace SpikeLite.IPC.WebHost.Services
+{
+    /// <summary>
+    /// Provides the service contract for our Ping service. We don't really have any namespace stuff setup, so use the gold ol' fashioned "tempuri."
+    /// </summary>
+    [ServiceContract(Namespace = "http://tempuri.org")]
+    public interface IWcfPingService
+    {
+        [OperationContract]
+        string Ping();    
+    }
+
+    /// <summary>
+    /// Implements our <see cref="IWcfPingService"/> contract.
+    /// </summary>
+    public class WcfPingService : IWcfPingService
+    {
+        public string Ping()
+        {
+            return "Pong!";
+        }
+    }
+}
---
+++ SpikeLite.IPC.WebHost/Services/WCFPingServiceHostModule.cs
@@ -0,0 +1,68 @@
+﻿/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2009 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+
+
+using SpikeLite.Modules;
+using log4net.Ext.Trace;
+using SpikeLite.Communications;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace SpikeLite.IPC.WebHost.Services
+{
+    /// <summary>
+    /// This class does all the WCF setup for our Ping webservice endpoint.
+    /// </summary>
+    public class WCFPingServiceHostModule : ModuleBase
+    {
+        /// <summary>
+        /// Stores our log4net logger.
+        /// </summary>
+        private static readonly TraceLogImpl _logger = (TraceLogImpl)TraceLogManager.GetLogger(typeof(WCFPingServiceHostModule));
+
+        /// <summary>
+        /// Register our WCF endpoint.
+        /// </summary>
+        /// 
+        ///
+        /// <remarks>
+        /// Users on Vista or greater might have to reserve the endpoint via Netsh.exe as follows:
+        /// 
+        /// <code>
+        /// netsh http add urlacl url=http://+:8080/SpikeLite/Ping user=DOMAIN\user 
+        /// </code>  
+        /// 
+        /// Please make sure to run your cmd session with administrative rights.
+        /// </remarks>
+        public WCFPingServiceHostModule()
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/SpikeLite/Ping");
+            ServiceHost selfHost = new ServiceHost(typeof(WcfPingService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IWcfPingService), new BasicHttpBinding(), "WcfPingService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior { HttpGetEnabled = true };
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+            }
+            catch (CommunicationException ce)
+            {
+                _logger.InfoFormat("Caught an exception trying to bind our WCF endpoint: {0}", ce);
+            }     
+        }
+
+        public override void HandleRequest(Request request)
+        {
+            //No-Op for now.
+        }
+    }
+}
