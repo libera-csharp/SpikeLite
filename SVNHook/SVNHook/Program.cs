e39133fdca58416dffef5be77cc9912d747b1271
Greg F <kog@epiphanic.org>
2009-12-09 12:47:05 +0000

    Add a Tools branch, commit the SVN <--> MessagingService bridge. I've tested it against Mono 2.6, as well as the MS CLR. It's still a little hackish, but it does the job and reads the metadata... which is more than I can say about Python or Perl. Perhaps the metadata is dirty, I'll need to put more research into it.

---
+++ SVNHook/SVNHook/Program.cs
@@ -0,0 +1,46 @@
+﻿/**
+ * SpikeLite C# IRC Bot
+ * Copyright (c) 2009 FreeNode ##Csharp Community
+ * 
+ * This source is licensed under the terms of the MIT license. Please see the 
+ * distributed license.txt for details.
+ */
+
+using SVNHook.MessagingService;
+
+namespace SVNHook
+{
+    public class Program
+    {
+        /// <summary>
+        /// Passes commit notices to the bot. There's a post-commit hook that passes in two params (wrapped in quotes): the channel target
+        /// and the commit message using SVNLook, that looks something like:  
+        /// 
+        /// <code>
+        /// "R143 [2009-12-09 04:36:11 -0800 (Wed, 09 Dec 2009)] kog - More whitespace. Let's test the commit hook one last time."
+        /// </code>
+        /// 
+        /// This is pretty simple, but it does the job for now.
+        /// </summary>
+        /// 
+        /// <param name="args">An array of strings such that the first argument is a channel target the bot is in, and the second is a commit message.</param>
+        /// 
+        /// <remarks>
+        /// This app was generated using MS' svcutil implementation. The mono implementation didn't seem to want to parse, and both SOAPpy and SOAP::Lite had
+        /// problems as well. I've kept the output mostly the same, except we currently don't use authentication/security on the WS endpoints due to Mono's 
+        /// lack of implementation. Not surprisingly, Mono can't have any of the security attributes in the app.config, so I stripped them out (not like
+        /// we had anything anyway).
+        /// </remarks>
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 1)
+            {
+                string target = args[0];
+                string message = args[1];
+
+                MessagingServiceClient proxy = new MessagingServiceClient();
+                proxy.SendMessage(target, message);
+            }
+        }
+    }
+}
---
+++ SVNHook/SVNHook/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SVNHook")]
+[assembly: AssemblyDescription("Allows SVN post-commit messages to message SpikeLite's MessageService.")]
+[assembly: AssemblyConfiguration("Simple as Possible")]
+[assembly: AssemblyCompany("The Freenode ##CSharp Community")]
+[assembly: AssemblyProduct("SVNHook")]
+[assembly: AssemblyCopyright("Copyright © The FreeNode ##CSharp Community 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
